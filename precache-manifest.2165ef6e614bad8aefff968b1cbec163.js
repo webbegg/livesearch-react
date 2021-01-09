d9828bb3c485f6995f59d9c85721c8f134f13195
Marcos Juan Juan <webbegg@gmail.com>
2021-01-09 16:34:44 +0100

    Updates

---
+++ precache-manifest.2165ef6e614bad8aefff968b1cbec163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f220bea4309d399b58f8ca3710d02ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb6a4bbafe5d5a1972c5",
+    "url": "./static/css/main.8a924caf.chunk.css"
+  },
+  {
+    "revision": "fac1d2b0468da7e233d5",
+    "url": "./static/js/2.a38b0e04.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.a38b0e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6a4bbafe5d5a1972c5",
+    "url": "./static/js/main.41f1270b.chunk.js"
+  },
+  {
+    "revision": "69f4ed428dbec8da654a",
+    "url": "./static/js/runtime-main.22360bae.js"
+  }
+]);
